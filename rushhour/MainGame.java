3a90a84e6ddefb4c78cd4952ca2d55c8046939f6
tetearie054 <tetearie054@gmail.com>
2023-06-08 08:24:34 -0400

    pause ui

--- rushhour/MainGame.java
+++ rushhour/MainGame.java
@@ -36,10 +36,17 @@ public class MainGame extends JFrame implements ActionListener{
     }
 
     final static int SCRW = 500, SCRH = 800;
-    Font f = new Font("Comic Sans MS", Font.BOLD, 30);
+    Font f = new Font("Monospaced", Font.BOLD, 30);
+    Font title = new Font("Monospaced", Font.BOLD, 50);
+    Color blue = new Color(167, 199, 231);
+    Color yellow = new Color(248, 241, 174);
+    Color darkBlue = new Color(61, 66, 107);
+    Color pink = new Color(242, 196, 229);
     DrawingPanel DP = new DrawingPanel();
     Timer t = new Timer(1, this);
-    int time = 0;
+    Timer counter = new Timer(1000, new Counter());
+    int sec = 0;
+    int min = 0;
     int moves = 0;
     MouseListener ML = new ML();
     Image pauseButton, restartButton;
@@ -83,6 +90,7 @@ public class MainGame extends JFrame implements ActionListener{
         this.setVisible(true);
         this.setLocationRelativeTo(null);
         t.start();
+        counter.start();
     }
 
     static BufferedImage loadImage(String filename) {
@@ -124,7 +132,7 @@ public class MainGame extends JFrame implements ActionListener{
     class DrawingPanel extends JPanel {
         DrawingPanel() {
             this.setPreferredSize(new Dimension(SCRW,SCRH));
-            this.setBackground(new Color(77,0,19));
+            this.setBackground(blue);
 
         }
 
@@ -135,16 +143,20 @@ public class MainGame extends JFrame implements ActionListener{
             Graphics2D g2 = (Graphics2D) g;
 			g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
             g2.setFont(f);
-            g2.setColor(Color.WHITE);
+            g2.setColor(pink);
 
 
             //moves, time
-            g2.drawString("Time: " + time, 50, 75);
+            g2.fillRoundRect(20,20,280, 110, 20, 20);
+            g2.setColor(darkBlue);
+            g2.drawString("Time: " + min + ":" + ((sec < 10) ? "0" : "") + sec, 50, 75);
             g2.drawString("Moves: " + moves, 275, 75);
 
             // Grid
+            g2.setColor(yellow);
             g2.fillRect(0, 150, 500, 500);
-            g2.setColor(Color.BLACK);
+            g2.setStroke(new BasicStroke(3));
+            g2.setColor(new Color(201, 183, 81));
             for(int i = 0; i < 6; i++) {
                 g2.drawLine(0, 150+SCRW/6*i, SCRW, 150+SCRW/6*i);
                 g2.drawLine(SCRW/6*i, 150, SCRW/6*i, 650);
@@ -157,15 +169,35 @@ public class MainGame extends JFrame implements ActionListener{
             g2.drawImage(restartButton, 350, 695, null);
 
             if(gamestate == GS.PAUSED){
-                g2.setStroke(new BasicStroke(10));
-                g2.setColor(Color.BLACK);
+
+                g2.setStroke(new BasicStroke(5));
+                g2.setColor(pink);
                 g2.fillRect(50, 100, 400,600);
-                g2.setColor(Color.WHITE);
-                g2.drawRect(50, 100, 400,600);
-                g2.drawString("PAUSED", 175,140);
+                g2.setColor(blue);
+                g2.fillRect(50, 100, 400,100);
+                g2.setFont(title);
+                g2.setColor(darkBlue);                
+
+                g2.drawString("PAUSED", 158,170);
+                g2.setFont(f);
+                g2.setColor(darkBlue);                
                 g2.drawString("Level: " + 3, 175, 250);
                 g2.drawString("Moves: " + moves, 175, 350);
-                g2.drawString("Time: " + time, 175, 450);
+                g2.drawString("Time: " + min + ":" + sec, 175, 450);
+                g2.setColor(yellow);
+                g2.fillRoundRect(80, 490, 340, 60, 20, 20); // 340 X 60 BACK TO GAME
+                g2.setColor(blue);
+                g2.fillRoundRect(80, 580, 180, 60, 20, 20);
+                g2.fillRoundRect(280, 580, 140, 60, 20, 20);
+                g2.setColor(darkBlue);
+                g2.drawString("Back To Game",140,530);
+                g2.setColor(yellow);
+
+                g2.drawString("Levels",115,620);
+                g2.drawString("Quit",312,620);
+                g2.setColor(darkBlue);
+                g2.drawRect(50, 100, 400,600);
+
             }
         }
     }
@@ -177,8 +209,6 @@ public class MainGame extends JFrame implements ActionListener{
             return;
         }
 
-
-        time++;
     }
 
 
@@ -187,11 +217,30 @@ public class MainGame extends JFrame implements ActionListener{
         @Override
         public void mouseClicked(MouseEvent e) {
             mx = e.getX(); my = e.getY();
+
+
+            // if they hit the pause button
             if(mx > 50 && mx < 150 && my > 680 && my < 780) {
-
-
-
                 gamestate = GS.PAUSED;
+            }
+            
+            // if they hit back to game while pasued --> continue
+            if(gamestate == GS.PAUSED) {
+                
+                // if they resume
+                if(mx > 80 && mx < 420 && my > 490 && my < 580) {
+                    gamestate = GS.PLAYING;
+                }
+
+                // if they hit levels
+                if(mx > 80 && mx < 260 && my > 580 && my < 640) {
+                    // gamestate = GS.LEVELS;
+                }
+
+                // if they hit quit
+                if(mx > 280 && mx < 420 && my > 580 && my < 640) {
+                    // QUIT CODE
+                }
             }
         }
 
@@ -206,6 +255,20 @@ public class MainGame extends JFrame implements ActionListener{
 
         @Override
         public void mouseExited(MouseEvent e) {}
+
+    }
+
+
+    class Counter implements ActionListener {
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            if(gamestate == GS.PLAYING) sec++;
+            if(sec == 60) {
+                min++;
+                sec = 0;
+            }
+        }
 
     }
      
